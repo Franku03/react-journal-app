62b167db57a102e62c2492b7b1c1a81c67325440
Franku <franklinquintana2003@gmail.com>
2025-01-02 18:15:46 -0400

    Inicio sección 17

---
+++ src/auth/pages/LoginPage.jsx
@@ -0,0 +1,50 @@
+import { Grid2, TextField, Typography } from "@mui/material"
+
+export const LoginPage = () => {
+  return (
+    <Grid2 
+      container
+      spacing={ 0 }
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+      sx={{ minHeight: '100vh', backgroundColor: 'primary.main', padding: 4}}
+    >
+      <Grid2 item
+        className='box-shadow'
+        xs={ 3 }
+        sx={{ backgroundColor: 'white', padding: 3, borderRadius: 2}}
+      >
+        <Typography variant='h5' sx={{ mb: 1}}>
+          Login
+        </Typography>
+
+        <form>
+
+        <Grid2 container>
+          <Grid2 item size={{ xs: 12, md: 12 }} sx={{ mt: 2 }}>
+            <TextField 
+              label="Correo" 
+              type="email" 
+              placeholder="correo@google.com"
+              fullWidth
+            />
+          </Grid2>
+
+          <Grid2 item size={{ xs: 12, md: 12 }} sx={{ mt: 2 }}>
+            <TextField 
+              label="Contraseña" 
+              type="password" 
+              placeholder="Contraseña"
+              fullWidth
+            />
+          </Grid2>
+        </Grid2>
+
+        </form>
+
+      </Grid2>
+
+    </Grid2>
+  )
+}
